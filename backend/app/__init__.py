dab909b88571dc7371803df53e1a1fcc0bf83653
Gian_Pereira <gian.21.otaku@gmail.com>
2024-12-23 20:47:55 -0300

    Doing some tests

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,12 +1,30 @@
 import flask
-
+import flask_socketio
+import flask_cors
 
 app = flask.Flask(__name__)
+cors = flask_cors.CORS(app, resources={r"/*": {"origins": "*"}})
+socket_io = flask_socketio.SocketIO(app, cors_allowed_origins="*")
 
+pressed_times = 0
 
-@app.route('/', methods=['get'])
+@app.route("/", methods=["GET"])
 def main():
-    return 'Hello'
+    return "Hello Sockets tests"
+
+@socket_io.on("connect")
+def handle_connection():
+    print("Cliente conectado")
+    global pressed_times
+    socket_io.emit("state_received", pressed_times)
+
+@socket_io.on("button_pressed")
+def handle_message(data):
+    print(f"Mensagem recebida: {data}")
+    global pressed_times
+    pressed_times += 1
+    socket_io.emit("state_received", pressed_times)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # Use `socket_io.run` ao invés de `app.run`
+    socket_io.run(app, debug=True, host="0.0.0.0", port=5000)
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,46 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { io } from "socket.io-client";
 
+// Certifique-se de usar o domínio correto do Ngrok
+const socket = io(process.env.REACT_APP_SERVER, {
+  transports: ["websocket", "polling"], // Transportes suportados
+});
+
+console.log(process.env.REACT_APP_SERVER)
 
 function App() {
   return (
-  <div>
-      <ButtonTest/>
-  </div>
+    <div>
+      <ButtonTest />
+    </div>
   );
 }
 
 function ButtonTest() {
-  const[pressedTimes, setPressed] = useState(0);
+  const [pressedTimes, setPressed] = useState(0);
 
   const handlePressed = () => {
-    setPressed(pressedTimes+1)
-  }
+    socket.emit("button_pressed", "button clicked");
+  };
+
+  useEffect(() => {
+    // Recebe atualizações de estado
+    socket.on("state_received", (pressed_times) => {
+      setPressed(pressed_times);
+    });
+
+    // Limpeza do evento
+    return () => {
+      socket.off("state_received");
+    };
+  }, []);
 
   return (
     <div>
-      <p>Vezes apertadas {pressedTimes}</p>
-      <button onClick={handlePressed}>
-        Aperta esse aqui
-      </button>
+      <p>Vezes apertadas: {pressedTimes}</p>
+      <button onClick={handlePressed}>Aperta esse aqui</button>
     </div>
-  )
+  );
 }
 
 export default App;
